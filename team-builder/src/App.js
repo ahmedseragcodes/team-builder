d70b7ea70dcde9731e2e6725d95cb07d5f4b43c1
ahmedseragcodes <ahmedserag1@gmail.com>
2020-11-11 09:18:15 -0500

    added basic form, working on build it out

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react"
+import ReactDOM from "react-dom"
+
+
 
 function App() {
+
+  const [teamMembers, setTeamMembers]=useState([])
+  const [teamMate, setTeamMate]=useState("")
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{teamMembers}</h1>
+      <AddMember />
     </div>
-  );
+    );
 }
+
+
+function AddMember() {
+
+
+
+  return (
+    <div>
+    <form>
+    <label htmlFor="name">
+      <input 
+      type="text"
+      id="name"
+      placeholder="name"
+      />
+    </label>
+  </form>
+  </div>
+  )
+}
+
+
+
+
+
 
 export default App;

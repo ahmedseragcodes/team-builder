7caba0c82b26e7b7260fd7056ea6978482a6a19e
ahmedseragcodes <ahmedserag1@gmail.com>
2021-01-26 16:22:15 -0500

    created basic heading, routing, and form layout

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+import React, { useState } from "react";
+import { Link, Route, BrowserRouter as Router } from "react-router-dom";
+import ReactDOM from "react-dom";
+
+const initialFormValues={
+  username: "",
+  email: "",
+  role: ""
+}
 
 function App() {
+
+const [formValues, setFormValues]=useState(initialFormValues);
+
+const onChange=function(event){
+  const { name, value }=event.target;
+
+  setFormValues({ ...formValues, [name]: value })
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DevTeam>
+      <h1>Our Dev Team</h1>
+        <Link to="/">Home</Link>
+      <form>
+        <label htmlFor="username">Username:
+          <input name="username" type="text" value={formValues.username} onChange={onChange} />
+        </label>
+        <label htmlFor="email">Email:
+          <input name="email" type="email" value={formValues.email} onChange={onChange} />
+        </label>
+        <label htmlFor="role">Role:
+          <select name="role" type="dropdown" value={formValues.role}>
+            <option value="FE">Front End Engineer</option>
+            <option value="BE">Back End Engineer</option>
+          </select>
+        </label>
+      </form>
+    </DevTeam>
   );
 }
+
+//Styled Components 
+
+const DevTeam=styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+font-size: 1.8rem;
+`
 
 export default App;
